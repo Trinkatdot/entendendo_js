922cecc426c749ddb58536422f154c9a19c23edb
Trinkatdot <agatha.ktaureliano@senacsp.edu.br>
2024-08-22 00:29:04 -0300

    feat: script de media com else if

--- script.js
+++ script.js
@@ -7,18 +7,27 @@ media < 4: reprovado.
 
 Obs use o prompt e exiba os resultado em alert.
 */
+
+//inicio
 let = nome1 = "";
-let = nota1, nota2, nota3 = 0;
+let nota1 = 0, nota2 = 0, nota3 = 0;
 
+/*verificar o porque esta errado
+let = nota1, nota2, nota3 = 0
+*/
+
+//entrada de dados
 nome1 = prompt("Digite o nome");
-nota1 = parseFloat(prompt("diite a 1° nota"));
-nota2 = parseFloat(prompt("diite a 2° nota"));
-nota3 = parseFloat(prompt("diite a 3° nota"));
+nota1 = parseFloat(prompt("digite a 1° nota"));
+nota2 = parseFloat(prompt("digite a 2° nota"));
+nota3 = parseFloat(prompt("digite a 3° nota"));
 
-let = media1 = 0;
- media1 = (nota1 + nota2 + nota3) /3;
+//calculo da media
+let media = (nota1 + nota2 + nota3) / 3;
 
-let = media = media1
-if (media > 6) { 
-    prompt("aluno aprovado")
+//logica do alerta 
+if (media > 6) {
+    alert(nome1 + ", esta aprovado");
+} else if (media < 4) {
+    alert(nome1 + ", esta reprovado");
 }
