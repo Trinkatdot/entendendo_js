264f0f44b5c8aa31a090785a62a61528d7319b80
Trinkatdot <agatha.ktaureliano@senacsp.edu.br>
2024-08-22 18:04:29 -0300

    feat: corrigido alerta final

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ Obs use o prompt e exiba os resultado em alert.
 */
 
 //inicio
-let = nome1 = "";
+let nome1 = "";
 let nota1 = 0, nota2 = 0, nota3 = 0;
 
 /*verificar o porque esta errado
@@ -26,8 +26,8 @@ nota3 = parseFloat(prompt("digite a 3° nota"));
 let media = (nota1 + nota2 + nota3) / 3;
 
 //logica do alerta 
-if (media > 6) {
-    alert(nome1 + ", esta aprovado");
-} else if (media < 4) {
-    alert(nome1 + ", esta reprovado");
+if (media >= 6) {
+    alert(nome1 + ", esta aprovado, parabéns!");
+} else if (media <= 4) {
+    alert(nome1 + ", esta reprovado, é uma pena.");
 }
